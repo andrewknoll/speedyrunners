4bdb2da02c8c43704f85b0a658940c0f095e6745
Andrew <aog25.2014@gmail.com>
2021-02-19 11:37:02 +0100

    Bouncing ball with resizable window

--- Videojuegos/main.cpp
+++ Videojuegos/main.cpp
@@ -1,19 +1,45 @@
 
 #include <SFML/Graphics.hpp>
+#include <random>
+#include <time.h>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
+	sf::CircleShape shape(20.f);
+	shape.setFillColor(sf::Color::Red);
+
+	window.setFramerateLimit(60); //60 FPS
+
+	srand(time(NULL));
+
+	float randX = static_cast<float>(5.0 * rand()/RAND_MAX), randY = static_cast<float>(5.0 * rand()/RAND_MAX);
 
 	while (window.isOpen())
 	{
+		if (shape.getPosition().x + shape.getRadius()*2 + randX > window.getSize().x
+			|| shape.getPosition().x < 0) {
+			randX = -randX;
+			
+		}
+		if (shape.getPosition().y + shape.getRadius() * 2 + randY > window.getSize().y
+			|| shape.getPosition().y < 0) {
+			randY = -randY;
+		}
+		
+		shape.move(randX, randY);
+		
+		
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::Resized)
+			{
+				sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+				window.setView(sf::View(visibleArea));
+			}
 		}
 
 		window.clear();
